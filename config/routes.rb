02215b338b017a9079c121894099b7f51429b63c
ysgim1111 <anjella6636@gmail.com>
2016-07-13 23:23:11 +0900

    first_draft

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+  get 'home/index'
+
+  get ':controller(/:action(/:id))'
+  post ':controller(/:action(/:id))'
+
+  root 'home#index'
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with "rake routes".
 
   root 'products#index'
 
