286aad20bce2b3b8524266b0f0518c1871402507
jamie <ogelacinyc@gmail.com>
2016-07-19 07:28:26 +0900

    디바이스 및 메일건 ENV 셋팅

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: 'trd.st', port: 80 }
   config.action_mailer.delivery_method = :mailgun
-  config.action_mailer.mailgun_settings = { api_key: ENV['MAILGUN_KEY'], domain: ENV['MAILGUN_DOMAIN'] }
+  config.action_mailer.mailgun_settings = { api_key: "#{ENV['MAILGUN_KEY']}", domain: "#{ENV['MAILGUN_DOMAIN']}" }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
