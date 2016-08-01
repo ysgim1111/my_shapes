1a7b8c572781b7e5acc46f851c8129734607cda4
jamie <ogelacinyc@gmail.com>
2016-08-01 20:33:40 +0900

    스팟딜 상품정보 가져오기

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
-  enum view_type: [:regular, :main, :deal]
+  enum view_type: [:regular, :main, :deal_upper, :deal]
 
   default_scope -> { where(enable: true) }
   scope :disable, -> { where(enable: false) }
