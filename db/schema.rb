3297ab0e85700e5429d94a5bc85ac636e573820c
jamie <ogelacinyc@gmail.com>
2016-08-04 05:44:12 +0900

    상품디테일변경  - 옵션+수량 마진탑 변경  - 상품 이미지 추가 필드
    인플루언서 스토어 변경
     - 이미지 슬라이드제거

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160803030415) do
+ActiveRecord::Schema.define(version: 20160803203628) do
 
   create_table "destinations", force: :cascade do |t|
     t.integer  "user_id"
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 20160803030415) do
     t.integer  "discount",          default: 0
     t.integer  "shipping_expenses", default: 0
     t.integer  "view_type",         default: 0,    null: false
+    t.string   "image_url2"
   end
 
   create_table "purchase_lists", force: :cascade do |t|
