83141d4740584b1c2c49d85d9b628d08af0608b5
KimYo-seop <anjella6636@gmail.com>
2016-08-16 16:16:03 +0900

    정리

--- db/schema.rb
+++ db/schema.rb
@@ -146,6 +146,16 @@ ActiveRecord::Schema.define(version: 20160815121913) do
 
   add_index "purchase_lists", ["user_id"], name: "index_purchase_lists_on_user_id"
 
+  create_table "purchase_products", force: :cascade do |t|
+    t.integer  "purchase_list_id"
+    t.integer  "product_id"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
+
+  add_index "purchase_products", ["product_id"], name: "index_purchase_products_on_product_id"
+  add_index "purchase_products", ["purchase_list_id"], name: "index_purchase_products_on_purchase_list_id"
+
   create_table "purchase_results", force: :cascade do |t|
     t.integer  "purchase_list_id"
     t.string   "imp_uid"
